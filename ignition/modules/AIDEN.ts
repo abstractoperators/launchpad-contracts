4d3643c1946c3b1c4715c6f28dfa121ce867c345
Michael Deng <michaeldeng2003@gmail.com>
2025-04-01 18:10:49 -0500

    Add FFactory, FRouter, and Bonding contracts to AIDEN ignition module.

--- ignition/modules/AIDEN.ts
+++ ignition/modules/AIDEN.ts
@@ -2,6 +2,11 @@ import { buildModule } from "@nomicfoundation/hardhat-ignition/modules";
 
 export default buildModule("AIDEN", (m) => {
   const wsei = m.contract("WSEI", []);
+  const ffactory = m.contract("FFactory", []);
+  const router = m.contract("FRouter", []);
+  const bonding = m.contract("Bonding", []);
+
+  // const 
 
 //   m.call(wsei, "withdraw", [1]);
 
