ea8aa42167011da76b3154196fedb097d7397561
Michael Deng <michaeldeng2003@gmail.com>
2025-04-01 18:00:25 -0500

    Install ignition

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,7 +1,7 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 import "@openzeppelin/hardhat-upgrades";
-
+import "@nomicfoundation/hardhat-ignition-ethers";
 const config: HardhatUserConfig = {
   solidity: {
     version: "0.8.28",
