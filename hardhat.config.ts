d8e1bbbae466461b5e139e706dfd2b7b5560db3a
Michael Deng <michaeldeng2003@gmail.com>
2025-04-02 01:12:14 -0500

    Add real testnet configurations to Hardhat + account specification

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -2,6 +2,8 @@ import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 import "@openzeppelin/hardhat-upgrades";
 import "@nomicfoundation/hardhat-ignition-ethers";
+const { vars }  = require("hardhat/config");
+const PRIVATE_KEY = vars.get("PRIVATE_KEY");
 const config: HardhatUserConfig = {
   solidity: {
     version: "0.8.28",
@@ -14,11 +16,18 @@ const config: HardhatUserConfig = {
     }
   },
   networks: {
-    testnet: {
+    local_testnet: {
       url: "http://127.0.0.1:8545",
       chainId: 31337, // This is the ChainID for forked atlantic-2
     },
+    // Documentation on deploying to a live network 
+    // https://hardhat.org/tutorial/deploying-to-a-live-network 
+    real_testnet: {
+      url: "https://evm-rpc-testnet.sei-apis.com",
+      chainId: 1328,
+      accounts: [PRIVATE_KEY],
+    }
   }
-};
+}
 
 export default config;
