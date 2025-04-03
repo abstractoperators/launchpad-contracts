13fbb3dc9c4be65e3b40be4a2db38fb4b575f6e9
Michael Deng <michaeldeng2003@gmail.com>
2025-04-03 06:20:14 -0500

    Deploy to real_testnet.

--- deployment-scripts/create-erc20-and-pair.ts
+++ deployment-scripts/create-erc20-and-pair.ts
@@ -6,35 +6,17 @@ async function main() {
     const [deployer] = await ethers.getSigners();
     const owner = deployer;
 
-    const Bonding = await ethers.getContractAt("Bonding", "0x202CCe504e04bEd6fC0521238dDf04Bc9E8E15aB");
-    // const Factory = await ethers.getContractAt("FFactory", "0x52aBD4Cb4D3770c5c92b736aA169b4923867c6BB");
-    // const WSEI = await ethers.getContractAt("WSEI", "0xae7CC55D9cF3bd6CcC72dc113369d3e5a9085d32");
+    const Bonding = await ethers.getContractAt("Bonding", "0xDdFF841E7bb9c2180D160eE5E11663ca127Fd21e");
 
-    // const TestTokenFactory = await ethers.getContractFactory("FERC20");
-    // const TestToken = await TestTokenFactory.deploy("TestToken", "TT", ethers.parseEther("1000000"), ethers.parseEther("10000"));
-    // await TestToken.waitForDeployment();
-    // console.log("TestToken deployed to:", TestToken.target);
-
-    
-    // console.log("Creating pair for token:", TestToken.target, "and WSEI:", WSEI.target);
-    // await Factory.connect(owner).createPair(TestToken.target, WSEI.target);
-
-    // const pairAddress = await Factory.getPair(TestToken.target, WSEI.target);
-    // console.log("Pair created at:", pairAddress);
 
     const launchFee = await Bonding.assetLaunchFee();
-    // // Make sure the 'user' has enough WSEI to pay for the launch fee by transferring WSEI to the user
-    // const transferResult = await WSEI.connect(owner).transfer(await user.getAddress(), launchFee)
-    // await transferResult.wait()
 
-    // await WSEI.connect(owner).approve(Bonding.target, launchFee);
     const launchTx = await Bonding.connect(owner).launchWithSei(
         "this is my token launched with sei",
         "t1",
         {
             value: launchFee
         }
-        // WSEI.target
     );
     const user = owner;
     const launchReceipt = await launchTx.wait();
